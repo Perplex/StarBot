9bed58051949f27f3330eb2791e569d721d77037
Ceegan Hale <ceeganhale@gmail.com>
2017-11-17 00:24:31 -0700

    Blink kiting

--- commandcenter-master/src/RangedManager.cpp
+++ commandcenter-master/src/RangedManager.cpp
@@ -45,7 +45,26 @@ void RangedManager::assignTargets(const std::vector<Unit> & targets)
                 if (m_bot.Config().KiteWithRangedUnits)
                 {
                     // TODO: implement kiting
-                    rangedUnit.attackUnit(target);
+					float distToEnemy = Util::Dist(rangedUnit, target);
+
+					if (distToEnemy > 5) {
+						rangedUnit.attackUnit(target);
+					}
+					else {
+						for (auto &tiles : m_bot.Map().getClosestTilesTo(rangedUnit.getTilePosition())) {
+							bool pass = false;
+							for (auto &unit : m_bot.GetUnits()){
+								if (unit.getTilePosition() == tiles) {
+									pass = true;
+									break;
+								}
+							}
+							if (!pass && Util::Dist(target, CCPosition((float) tiles.x, (float) tiles.y)) > distToEnemy + 4) {
+								rangedUnit.move(tiles);
+								break;
+							}
+						}
+					}
                 }
                 else
                 {
